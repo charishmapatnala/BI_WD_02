349d98087aafb5a3a41875e2c2b05ccdf3ca82a4
CHERRY <patnalacharishma27@gmail.com>
2024-06-11 16:45:28 +0530

    bi t2

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const app = express();
+
+mongoose.connect('mongodb://localhost/bharatInternDatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Corrected views directory path
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+// Root route to display all articles
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' });
+  res.render('articles/index', { articles: articles });
+});
+
+app.use('/articles', articleRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
